6389cbf4b480024d0882a1dca704fce348ef4fba
mjisan <jisan.mansur@gmail.com>
2024-05-02 15:48:19 -0400

    Update INIT directory with new parameters

--- INIT/boundary_home/scripts/boundary_parametric_model/read_details.m
+++
@@ -1,9 +0,0 @@
-function [x,y,x_center, y_center] = read_details(fi)
-%Read parameters from par
-% x,y,x_center, y_center
-x = ncread(fi, 'x_axis');
-y = ncread(fi, 'y_axis');
-x_center = ncread(fi, 'x_center');
-y_center = ncread(fi, 'y_center');
-end
-
